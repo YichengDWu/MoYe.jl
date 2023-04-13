de5f5e12bd4d23448ca86ab109f5f616a5600913
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-04-13 14:12:27 -0600

    Update CuTe.jl

--- src/CuTe.jl
+++ src/CuTe.jl
@@ -3,8 +3,11 @@ module CuTe
 include("algorithm/tuple_algorithms.jl")
 include("int_tuple.jl")
 include("stride.jl")
+include("layout.jl")
 
 export flatten
 export colex_less, elem_less, increment
 export coord_to_index, index_to_coord
+export Layout, make_layout
+
 end
