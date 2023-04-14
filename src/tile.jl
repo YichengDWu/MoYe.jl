a3c7fee018808525287f32e70cd4233c497da8de
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-04-13 22:26:53 -0600

    Create tile.jl

---
+++ src/tile.jl
@@ -0,0 +1,9 @@
+const Tile = Tuple{Vararg{Union{Int, Layout}}}
+
+#function tile(l1::Layout, l2::Layout)
+#    return tiled_divide(l1, l2)
+#end
+
+#function tile(l1::Layout, l2::Layout, l3::Layout...)
+#    return tiled_divide(l1, (l2, l3...))
+#end
