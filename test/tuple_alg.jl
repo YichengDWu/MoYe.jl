8f76b29cc87a77e631534d061c674e7a03ed331b
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-04-15 16:24:14 -0600

    Update tuple_alg.jl

--- test/tuple_alg.jl
+++ test/tuple_alg.jl
@@ -1,5 +1,6 @@
 using CuTe, Test
 
-@testset "zip2_by" begin CuTe.zip2_by((('A', 'a'), (('B', 'b'), ('C', 'c')), 'd'),
-                                      (0, (0, 0))) ==
-                         (('A', ('B', 'C')), ('a', ('b', 'c'), 'd')) end
+@testset "zip2_by" begin
+    @test CuTe.zip2_by((('A', 'a'), (('B', 'b'), ('C', 'c')), 'd'), (0, (0, 0))) ==
+          (('A', ('B', 'C')), ('a', ('b', 'c'), 'd'))
+end
