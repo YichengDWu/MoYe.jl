2a59ab7a3290ce727c8c538e2917444cfa5e9334
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-04-19 18:39:57 -0600

    relax type to  DenseVector

--- src/cutearray.jl
+++ src/cutearray.jl
@@ -1,7 +1,7 @@
-struct CuTeArray{T, N, E <: Engine{T}, L <: Layout{N}} <: AbstractArray{T, N}
+struct CuTeArray{T, N, E <: DenseVector{T}, L <: Layout{N}} <: AbstractArray{T, N}
     engine::E
     layout::L
-    function CuTeArray(engine::Engine{T}, layout::Layout{N}) where {T, N}
+    function CuTeArray(engine::DenseVector{T}, layout::Layout{N}) where {T, N}
         return new{T, N, typeof(engine), typeof(layout)}(engine, layout)
     end
 end
@@ -9,10 +9,15 @@ end
 engine(x::CuTeArray) = getfield(x, :engine)
 layout(x::CuTeArray) = getfield(x, :layout)
 
-#function Adapt.adapt_structure(to, x::CuTeArray)
-#    data = Adapt.adapt_structure(to, engine(x))
-#    return CuTeArray(data, layout(x))
-#end
+Base.size(x::CuTeArray) = map(capacity, shape(layout(x)))
+Base.length(x::CuTeArray) = length(engine(x))
+Base.getindex(x::CuTeArray, ids...) = getindex(engine(x), layout(x)(ids))
 
-#Adapt.adapt_storage(::Type{CuTeArray{T,N,A}}, xs::AT) where {T,N,A,AT<:AbstractArray} =
-#    Adapt.adapt_storage(A, xs)
+
+function Adapt.adapt_structure(to, x::CuTeArray)
+    data = Adapt.adapt_structure(to, engine(x))
+    return CuTeArray(data, layout(x))
+end
+
+Adapt.adapt_storage(::Type{CuTeArray{T,N,A}}, xs::AT) where {T,N,A,AT<:AbstractArray} =
+    Adapt.adapt_storage(A, xs)
