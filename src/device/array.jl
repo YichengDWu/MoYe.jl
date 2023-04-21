0e47a2f3959b798337feee8a064e44c67ce566c4
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-04-20 20:33:04 -0600

    Update array.jl

--- src/device/array.jl
+++ src/device/array.jl
@@ -14,3 +14,5 @@ end
 
 ## Shared Memory, static allocation but the layout can be dynamic
 @inline SharedMemory(T, ::StaticInt{L}) where {L} = CUDA.emit_shmem(T, Val(L))
+
+# TODO: mimic getindex and setindex! for CuDeviceArray
