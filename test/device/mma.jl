c1ad8e50a90a52011dba32970f146991c9e16e8d
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-04-22 10:36:15 -0600

    update

--- test/device/mma.jl
+++ test/device/mma.jl
@@ -1,21 +1,24 @@
 using Test, CuTe, CUDA
 
-@inline CuTe.Registers{T,S}() where {T, S} = ArrayEngine{T}(undef, static(S))
+if CUDA.functional()
+    @inline CuTe.Registers{T,S}() where {T, S} = ArrayEngine{T}(undef, static(S))
 
-function kernel()
-    op = MMA_16x8x16_F16F16F16F16_TN()
-    a_frag = op.ARegisters()
-    b_frag = op.BRegisters()
-    c_frag = op.CRegisters()
+    function kernel()
+        op = MMA_16x8x16_F16F16F16F16_TN()
+        a_frag = op.ARegisters()
+        b_frag = op.BRegisters()
+        c_frag = op.CRegisters()
 
-    d_frag = mma(op, a_frag, b_frag, c_frag) # this should be a ArrayEngine in the future
+        d_frag = mma(op, a_frag, b_frag, c_frag) # this should be a ArrayEngine in the future
 
-    s = d_frag[1][1].value
-    @cushow Float32(s)
-    return
+        s = d_frag[1][1].value
+        @cushow Float32(s)
+        return
+    end
+
+    buf = IOBuffer()
+    @device_code_ptx io = buf @cuda threads=32 kernel()
+    asm = String(take!(copy(buf)))
+    @test occursin("mma.sync.aligned.m16n8k16.row.col.f16.f16.f16.f16", asm)
+
 end
-
-buf = IOBuffer()
-@device_code_ptx io = buf @cuda threads=32 kernel()
-asm = String(take!(copy(buf)))
-@test occursin("mma.sync.aligned.m16n8k16.row.col.f16.f16.f16.f16", asm)
