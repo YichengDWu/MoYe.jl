229541150759b3207b3d3a458f9762f2315bf574
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-04-29 15:04:29 -0600

    Update copy_async.jl

--- test/device/copy_async.jl
+++ test/device/copy_async.jl
@@ -4,9 +4,8 @@ function copy_kernel(M, N, dest, src, blocklayout, threadlayout)
     smem = Shambles.SharedMemory(eltype(dest), cosize(blocklayout))
     cute_smem = CuTeArray(smem, blocklayout)
 
-
-    cute_dest = CuTeArray(pointer(dest), make_layout((256, 32))) # bug: cannot use (M, N)
-    cute_src = CuTeArray(pointer(src), make_layout((256, 32)))
+    cute_dest = CuTeArray(pointer(dest), Layout((M, N), (static(1), M))) # bug: cannot use make_layout((M, N))
+    cute_src = CuTeArray(pointer(src), Layout((M, N), (static(1), M)))
 
     bM = size(blocklayout, 1)
     bN = size(blocklayout, 2)
@@ -18,26 +17,31 @@ function copy_kernel(M, N, dest, src, blocklayout, threadlayout)
     threadtile_src = local_partition(blocktile_src, threadlayout, Int(threadIdx().x))
     threadtile_smem = local_partition(cute_smem, threadlayout, Int(threadIdx().x))
 
-   #cucopyto!(threadtile_smem, threadtile_src) # bug
+    cucopyto!(threadtile_smem, threadtile_src) # bug
     sync_threads()
-   # cucopyto!(threadtile_dest, threadtile_smem)
-  #  sync_threads()
+    cucopyto!(threadtile_dest, threadtile_smem)
+    sync_threads()
     return nothing
 end
 
-M = 256
-N = 32
+function test_copy_async()
+    M = 256
+    N = 32
 
-a = CUDA.rand(Float32, M, N)
-b = CUDA.rand(Float32, M, N)
+    a = CUDA.rand(Float32, M, N)
+    b = CUDA.rand(Float32, M, N)
 
-blocklayout = @Layout((128, 16))
-threadlayout = @Layout((32, 8))
+    blocklayout = @Layout((128, 16))
+    threadlayout = @Layout((32, 8))
 
-bM = size(blocklayout, 1)
-bN = size(blocklayout, 2)
+    bM = size(blocklayout, 1)
+    bN = size(blocklayout, 2)
 
-blocks = (cld(M, bM), cld(N, bN))
-threads = Shambles.Static.dynamic(size(threadlayout))
+    blocks = (cld(M, bM), cld(N, bN))
+    threads = Shambles.Static.dynamic(size(threadlayout))
 
-@cuda blocks=blocks threads=threads copy_kernel(M,N,a, b, blocklayout, threadlayout)
+    @cuda blocks=blocks threads=threads copy_kernel(M, N, a, b, blocklayout, threadlayout)
+    @test a == b
+end
+
+test_copy_async()
