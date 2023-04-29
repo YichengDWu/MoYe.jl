21eac0e1d69dc49ed27dd50e3fa92200214496fc
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-04-28 22:55:20 -0600

    Update copy_async.jl

--- test/device/copy_async.jl
+++ test/device/copy_async.jl
@@ -1,11 +1,12 @@
 using Shambles, Test, CUDA
 
-function copy_kernel(dest, src, blocklayout, threadlayout)
+function copy_kernel(M, N, dest, src, blocklayout, threadlayout)
     smem = Shambles.SharedMemory(eltype(dest), cosize(blocklayout))
     cute_smem = CuTeArray(smem, blocklayout)
 
-    cute_dest = CuTeArray(pointer(dest), @Layout((256, 32))) # bug: has to be static
-    cute_src = CuTeArray(pointer(src), @Layout((256, 32)))
+
+    cute_dest = CuTeArray(pointer(dest), make_layout((256, 32))) # bug: cannot use (M, N)
+    cute_src = CuTeArray(pointer(src), make_layout((256, 32)))
 
     bM = size(blocklayout, 1)
     bN = size(blocklayout, 2)
@@ -17,7 +18,7 @@ function copy_kernel(dest, src, blocklayout, threadlayout)
     threadtile_src = local_partition(blocktile_src, threadlayout, Int(threadIdx().x))
     threadtile_smem = local_partition(cute_smem, threadlayout, Int(threadIdx().x))
 
-    cucopyto!(threadtile_smem, threadtile_src) # bug
+   #cucopyto!(threadtile_smem, threadtile_src) # bug
     sync_threads()
    # cucopyto!(threadtile_dest, threadtile_smem)
   #  sync_threads()
@@ -39,4 +40,4 @@ bN = size(blocklayout, 2)
 blocks = (cld(M, bM), cld(N, bN))
 threads = Shambles.Static.dynamic(size(threadlayout))
 
-@cuda blocks=blocks threads=threads copy_kernel(a, b, blocklayout, threadlayout)
+@cuda blocks=blocks threads=threads copy_kernel(M,N,a, b, blocklayout, threadlayout)
