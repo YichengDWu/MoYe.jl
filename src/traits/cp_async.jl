65914b43ced44a85a92eb47a08e206d4e5d98a45
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-05-04 14:03:13 -0600

    reorganize folders

--- src/traits/cp_async.jl
+++ src/traits/cp_async.jl
@@ -1,20 +1,3 @@
-struct CopyTraits{C <: CPOP, TS, TD, LT, LS, LD, LR}
-    threadid::LT
-    srclayout::LS
-    dstlayout::LD
-    reflayout::LR
-end
-
-function CopyTraits{OP}(threadid, srclayout, dstlayout, reflayout=srclayout) where {S, D, OP<:CPOP{S,D}}
-    return CopyTraits{OP, S, D, typeof(threadid), typeof(srclayout), typeof(dstlayout), typeof(reflayout)}(threadid, srclayout, dstlayout, reflayout)
-end
-
-function CopyTraits{CPOP_UNIVERSAL{S,D}}() where {S,D}
-    threadid = make_layout(One())  # 1 thread per operation
-    srclayout = make_layout((One(), static(sizeof(S) * 8))) # thr -> bit
-    dstlayout = make_layout((One(), static(sizeof(D) * 8)))
-    return CopyTraits{CPOP_UNIVERSAL{S,D}}(threadid, srclayout, dstlayout)
-end
 
 function CopyTraits{CPOP_ASYNC_CACHEALWAYS{S,D}}() where {S,D}
     threadid = make_layout(One()) # 1 thread per operation
