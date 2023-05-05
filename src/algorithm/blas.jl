6daf625f1a784aa6f98a2f3de28a5b165c79fa00
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-05-04 21:40:09 -0600

    Update blas.jl

--- src/algorithm/blas.jl
+++ src/algorithm/blas.jl
@@ -10,6 +10,7 @@ end
 
 # used for dispatching
 const LocalArray{T,N} = MoYeArray{T,N, ViewEngine{T, Ptr{T}}}
+const SharedArray{T,N} = MoYeArray{T,N, ViewEngine{T, LLVMPtr{T, AS.Shared}}}
 
 gemm!(A::MoYeArray, B::MoYeArray, C::MoYeArray) = gemm!(C,A,B,C)
 #gemm!(mma::MMAAtom, A::MoYeArray, B::MoYeArray, C::MoYeArray) = gemm!(mma,C,A,B,C)
@@ -29,5 +30,38 @@ end
 function gemm!(mma_atom::MMAAtom, D::LocalArray{DT,2},  A::LocalArray{DA,1},
                B::LocalArray{DB,1}, C::LocalArray{DC,2}) where {DT,DA,DB,DC}
 
-    gemm(mma_atom, D, unsqueeze(A, StaticInt{2}()), unsqueeze(B, StaticInt{2}()), C)
+    gemm(mma_atom, D, append_dim(A, StaticInt{2}()), append_dim(B, StaticInt{2}()), C)
+end
+
+# matrix multiplication
+function gemm!(mma_atom::MMAAtom, D::LocalArray{DT,2},  A::LocalArray{DA,2},
+               B::LocalArray{DB,2}, C::LocalArray{DC,2}) where {DT,DA,DB,DC}
+    gemm(mma_atom,
+         prepend(D,  StaticInt{3}()), prepend(A,  StaticInt{3}()),
+         prepend(B,  StaticInt{3}()), prepend(C,  StaticInt{3}()))
+end
+
+# batched outer product
+function gemm!(mma_atom::MMAAtom, D::LocalArray{DT,3},  A::LocalArray{DA,2},
+               B::LocalArray{DB,2}, C::LocalArray{DC,3}) where {DT,DA,DB,DC}
+    M = size(A, 2)
+    N = size(B, 2)
+    @loopinfo unroll for n in 1:N
+        @loopinfo unroll for m in 1:M
+            ms = (n & 1) ? m : M+1-m
+            gemm(mma_atom, view(D, :, ms, n), view(A, :, ms), view(B, :, n), view(C, :, ms, n))
+        end
+    end
+end
+
+# batched matrix multiplication
+function gemm!(mma_atom::MMAAtom, D::LocalArray{DT,3},  A::LocalArray{DA,3},
+               B::LocalArray{DB,3}, C::LocalArray{DC,3}) where {DT,DA,DB,DC}
+    @loopinfo unroll for k in axes(A, 3)
+        gemm!(mma_atom,  D, view(A, :, :, k), view(B, :, :, k), C)
+    end
+end
+
+function gemm!()
+
 end
