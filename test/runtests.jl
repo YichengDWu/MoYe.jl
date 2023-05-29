77055c938fa770d3c327813dd0e24acf5f77b67e
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-05-29 12:57:46 -0600

    properly test

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,8 +18,9 @@ end
     @safetestset "Memory" begin include("device/memory.jl") end
     @safetestset "MMA" begin include("device/mmaop.jl") end
     @safetestset "MMATraits" begin include("device/mmatraits.jl") end
-    @safetestset "Pointer" begin include("device/pointer.jl") end
+    @safetestset "Pointer" begin include("device/pointer.jl") endss
     @safetestset "LDMatrix" begin include("device/ldmatrix.jl") end
+    @safetestset "Broadcast" begin include("device/broadcast.jl") end
 end
 
 @testset "Examples" begin
