5c36c006ef9f0904e4c898c7d6e6b8338a514d25
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-06-16 11:13:22 -0600

    Update blas.jl

--- src/algorithm/blas.jl
+++ src/algorithm/blas.jl
@@ -27,7 +27,7 @@ function gemm!(mma_atom::AbstractMMAAtom, D::LocalArray{DT,2},  A::LocalArray{DA
                B::LocalArray{DB,1}, C::LocalArray{DC,2}) where {DT,DA,DB,DC}
     @assert size(A.layout, 1) == size(C.layout, 1) == size(D.layout, 1) # M
     @assert size(B.layout, 1) == size(C.layout, 2 )== size(D.layout, 2) # N
-    gemm(mma_atom, D, append_dim(A, StaticInt{2}()), append_dim(B, StaticInt{2}()), C)
+    gemm!(mma_atom, D, append_dim(A, StaticInt{2}()), append_dim(B, StaticInt{2}()), C)
 end
 
 # matrix multiplication (2,2,2,2) -> (3,3,3,3)
@@ -37,7 +37,7 @@ function gemm!(mma_atom::AbstractMMAAtom, D::LocalArray{DT,2},  A::LocalArray{DA
     @assert size(B.layout, 1) == size(C.layout, 2) == size(D.layout, 2) # N
     @assert size(A.layout, 2) == size(B.layout, 2) # K
 
-    gemm(mma_atom,
+    gemm!(mma_atom,
          prepend_dim(D,  StaticInt{3}()), prepend_dim(A,  StaticInt{3}()),
          prepend_dim(B,  StaticInt{3}()), prepend_dim(C,  StaticInt{3}()))
 end
@@ -54,7 +54,7 @@ function gemm!(mma_atom::AbstractMMAAtom, D::LocalArray{DT,3},  A::LocalArray{DA
     @loopinfo unroll for n in One():N
         @loopinfo unroll for m in One():M
             ms = (n & 1) ? m : M+1-m
-            gemm(mma_atom, view(D, :, ms, n), view(A, :, ms), view(B, :, n), view(C, :, ms, n))
+            gemm!(mma_atom, view(D, :, ms, n), view(A, :, ms), view(B, :, n), view(C, :, ms, n))
         end
     end
 end
@@ -83,7 +83,7 @@ function gemm!(mma_atom::AbstractMMAAtom, D::LocalArray{DT,2},  A::SharedArray{D
     @assert size(mma_atom.traits.Blayout, 2) == One()
     @assert size(mma_atom.traits.Clayout, 2) == One()
 
-    gemm(mma_atom,
+    gemm!(mma_atom,
          prepend_dim(D,  StaticInt{3}()), prepend_dim(A,  StaticInt{3}()),
          prepend_dim(B,  StaticInt{3}()), prepend_dim(C,  StaticInt{3}()))
 end
