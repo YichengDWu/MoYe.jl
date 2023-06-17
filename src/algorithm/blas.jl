bfc072c3d0f516f3957ab458c3fd635c88523bfb
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-06-16 20:05:40 -0600

    Update blas.jl

--- src/algorithm/blas.jl
+++ src/algorithm/blas.jl
@@ -2,7 +2,7 @@
     x, y = ManualMemory.preserve_buffer(X), ManualMemory.preserve_buffer(Y)
     vx, vy = ViewEngine(pointer(X)), ViewEngine(pointer(Y))
     GC.@preserve x y begin
-        @loopinfo unroll  for i in eachindex(vx)
+        @loopinfo unroll for i in eachindex(vx)
             @inbounds vy[i] = iszero(b) ? a * vx[i] : a * vx[i] + b * vy[i]
         end
     end
