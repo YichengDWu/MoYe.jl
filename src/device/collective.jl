aeab9913ea4eecacc24dbf74637d842fc62874a5
Ethan Wu <yicheng.wu@ucalgary.ca>
2023-12-10 18:04:51 +0800

    restrict collective marco to be on the device

--- src/device/collective.jl
+++ src/device/collective.jl
@@ -1,4 +1,4 @@
-@inline function collective_copyto!(tiled_copy, dest, src)
+@device_override @inline function collective_copyto!(tiled_copy, dest, src)
     return quote
         thr_copy = get_thread_slice($(esc(tiled_copy)), Int(threadIdx().x))
         thr_D = partition_D(thr_copy, $(esc(dest)))
@@ -11,7 +11,7 @@ end
 """
     @collective tiled_xxx f(args...)
 
-Thread block level collective operation.
+Thread block level collective operation. This macro can only be used on the device.
 """
 macro collective(tiled_copy, ex)
     @capture(ex, f_(args__)) || error("unexpected expression")
