4e5a287b841f9021b17efecaeaa33be437bb6f4c
Ethan Wu <yicheng.wu@ucalgary.ca>
2024-04-25 01:43:21 -0600

    Update collective.jl

--- src/device/collective.jl
+++ src/device/collective.jl
@@ -1,6 +1,6 @@
 @inline function collective_copyto!(tiled_copy, dest, src)
     return quote
-        thr_copy = get_slice($(esc(tiled_copy)), Int(threadIdx().x))
+        thr_copy = get_slice($(esc(tiled_copy)), threadIdx().x)
         thr_D = partition_D(thr_copy, $(esc(dest)))
         thr_S = partition_S(thr_copy, $(esc(src)))
 
