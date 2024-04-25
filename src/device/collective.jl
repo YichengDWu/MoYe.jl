e5397ad876a95cc35f1fc97c1af6405988f959e0
Ethan Wu <yichengdwu@outlook.com>
2024-04-25 01:42:28 -0600

    Revert "Delete src/device/collective.jl"
    This reverts commit 66624a166b3ed9cfb3cdc9659624680c5dbe2202.

---
+++ src/device/collective.jl
@@ -0,0 +1,23 @@
+@inline function collective_copyto!(tiled_copy, dest, src)
+    return quote
+        thr_copy = get_slice($(esc(tiled_copy)), Int(threadIdx().x))
+        thr_D = partition_D(thr_copy, $(esc(dest)))
+        thr_S = partition_S(thr_copy, $(esc(src)))
+
+        copyto!($(esc(tiled_copy)), thr_D, thr_S)
+    end
+end
+
+"""
+    @collective tiled_xxx f(args...)
+
+Thread block level collective operation.
+"""
+macro collective(tiled_copy, ex)
+    @capture(ex, f_(args__)) || error("unexpected expression")
+    if f == :copyto!
+        # 解析 args__ 以获得 dest 和 src
+        @capture(args__, (dest_, src_)) || error("unexpected arguments for copyto!")
+        return collective_copyto!(tiled_copy, dest_, src_)
+    end
+end
