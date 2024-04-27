06080e263cede929d9a27ead40039a34b1206663
Ethan Wu <yicheng.wu@ucalgary.ca>
2024-04-27 16:22:37 -0700

    assume ptrs are already recasted

--- src/arch/copy/copy.jl
+++ src/arch/copy/copy.jl
@@ -27,32 +27,25 @@ struct UniversalCopy{TS, TD} <: AbstractCopyOp{Registers{TS, 1}, Registers{TD, 1
 
 @inline UniversalCopy{S}() where {S} = UniversalCopy{S,S}()
 
-function (::UniversalCopy{TS, TD})(dest::LLVMPtr, src::LLVMPtr) where {TS, TD}
+function (::UniversalCopy{TS, TD})(dest::LLVMPtr{TD}, src::LLVMPtr{TS}) where {TS, TD}
     @inline
-    src = recast(TS, src)
-    dest = recast(TD, dest)
     align_src = Base.datatype_alignment(TS)
     align_dst = Base.datatype_alignment(TD)
-
     return unsafe_store!(dest, unsafe_load(src, 1, Val(align_src)), 1, Val(align_dst))
 end
 
 # the following methods should be moved if LocalArray has an address space
-function (::UniversalCopy{TS, TD})(dest::Ptr, src::Ptr) where {TS, TD}
+function (::UniversalCopy{TS, TD})(dest::Ptr{TD}, src::Ptr{TS}) where {TS, TD}
     @inline
     src = recast(TS, src)
     dest = recast(TD, dest)
     return unsafe_store!(dest, unsafe_load(src))
 end
-function (::UniversalCopy{TS, TD})(dest::Ptr, src::LLVMPtr) where {TS, TD}
+function (::UniversalCopy{TS, TD})(dest::Ptr{TD}, src::LLVMPtr{TS}) where {TS, TD}
     @inline
-    src = recast(TS, src)
-    dest = recast(TD, dest)
     return unsafe_store!(dest, unsafe_load(src, 1, Val(Base.datatype_alignment(TS))))
 end
-function (::UniversalCopy{TS, TD})(dest::LLVMPtr, src::Ptr) where {TS, TD}
+function (::UniversalCopy{TS, TD})(dest::LLVMPtr{TD}, src::Ptr{TS}) where {TS, TD}
     @inline
-    src = recast(TS, src)
-    dest = recast(TD, dest)
     return unsafe_store!(dest, unsafe_load(src), 1, Val(Base.datatype_alignment(TD)))
 end
